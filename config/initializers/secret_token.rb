713430539b299af351d4e8ec9e841e1a3bf4745d
Neal Peters <nealcpeters@gmail.com>
2014-04-09 20:04:33 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Happiness::Application.config.secret_key_base = 'e0bbd4cfa846ecf03c5f249396905f6da0dbbd3a5bb643e3c46533a51cbe2efad4b4286dd9ab961e4d2e4bbe69bf8e8ae603dbaf5d71a49081765620c1a17937'
