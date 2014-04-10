27481939217c67ed3d31264be0a092628e49c21d
Eric Stensland <estensland88@gmail.com>
2014-04-09 21:20:25 -0500

    fixed seed and migration

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,7 @@ Image.create(path: "http://s3-ak.buzzfeed.com/static/enhanced/terminal01/2011/9/
 Image.create(path: "http://s3-ak.buzzfeed.com/static/enhanced/terminal01/2011/9/30/14/enhanced-buzz-19891-1317409057-7.jpg")
 Image.create(path: "http://s3-ak.buzzfeed.com/static/enhanced/terminal01/2011/9/30/15/enhanced-buzz-19889-1317412032-12.jpg")
 Image.create(path: "http://s3-ec.buzzfed.com/static/enhanced/webdr03/2013/3/12/15/anigif_enhanced-buzz-13541-1363117005-5.gif")
-Image.create(path: "http://s3-ec.buzzfed.com/static/enhanced/webdr02/2013/3/12/15/anigif_enhanced-buzz-19048-1363116994-4.gif"
+Image.create(path: "http://s3-ec.buzzfed.com/static/enhanced/webdr02/2013/3/12/15/anigif_enhanced-buzz-19048-1363116994-4.gif")
 Image.create(path: "http://s3-ec.buzzfed.com/static/enhanced/webdr02/2013/3/12/17/anigif_enhanced-buzz-1934-1363122302-1.gif")
 Image.create(path: "http://smileplez.s3.amazonaws.com/images/2013/August/10/5206e2d8d37b8.jpg")
 Image.create(path: "http://i.imgur.com/l9JA8.gif")
