feb4db2a9ee13ad1292c987283d3d2c3f2983dab
Eric Stensland <estensland88@gmail.com>
2014-04-09 20:55:25 -0500

    woot

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Image.create(path: "http://s3-ak.buzzfeed.com/static/enhanced/terminal01/2011/9/30/12/enhanced-buzz-19887-1317398816-9.jpg")
+Image.create(path: "http://s3-ak.buzzfeed.com/static/enhanced/terminal01/2011/9/30/14/enhanced-buzz-19891-1317409057-7.jpg")
+Image.create(path: "http://s3-ak.buzzfeed.com/static/enhanced/terminal01/2011/9/30/15/enhanced-buzz-19889-1317412032-12.jpg")
