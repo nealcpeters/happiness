8772157eebf870ef73855bd9f4c24c48af688261
Eric Stensland <estensland88@gmail.com>
2014-04-23 08:12:15 -0500

    both up and down jubilation arrows are locked

--- app/assets/javascripts/home_script.js
+++ app/assets/javascripts/home_script.js
@@ -1,9 +1,19 @@
 $(function(){
+
   $('#jub-submit').on('click', function(e){
     e.preventDefault();
-    data = {jubilation: {content: $('#jubilation_content').val()}}
+    data = {jubilation: {content: $('#jubilation_content').val()}};
     $.post("/jubilations", data, function(reply){
-      $('#jub-container').html(reply)
-    })
-  })
-})
+      $('#jub-container').html(reply);
+    });
+  });
+
+  $(document).on('click', '.jub-arrow', function(e){
+    e.preventDefault();
+    url = this.href;
+    $.get(url, function(reply){
+      $('#jub-container').html(reply);
+    });
+  });
+});
+
